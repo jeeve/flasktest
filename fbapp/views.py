e6bd3a7a65ebdd3a5caadf7f6908658e7c7924d6
jeeve <jyvaisj@gmail.com>
2020-12-04 21:44:04 +0100

    plot

--- fbapp/views.py
+++ fbapp/views.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, url_for, request
 
+import io
+import random
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 app = Flask(__name__)
 
 # Config options - Make sure you created a 'config.py' file.
@@ -11,8 +17,8 @@ from .utils import OpenGraphImage
 @app.route('/')
 @app.route('/index/')
 def index():
-    page_title = "Le super  test flask"
-    return render_template('index.html')
+    page_title = "Le super test flask cool"
+    return render_template('index.html', page_title = page_title)
 
 @app.route('/result/')
 def result():
@@ -21,3 +27,18 @@ def result():
 # @app.route('/contents/<int:content_id>/')
 # def content(content_id):
 #     return '%s' % content_id
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
