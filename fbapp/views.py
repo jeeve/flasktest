ae57973024c595cd624fd28193f5be72795ddfe9
jeeve <jyvaisj@gmail.com>
2020-12-05 16:08:05 +0100

    p

--- fbapp/views.py
+++ fbapp/views.py
@@ -6,6 +6,11 @@ from flask import Response
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+import numpy as np
+
 app = Flask(__name__)
 
 # Config options - Make sure you created a 'config.py' file.
@@ -36,9 +41,26 @@ def plot_png():
     return Response(output.getvalue(), mimetype='image/png')
 
 def create_figure():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    xs = range(100)
-    ys = [random.randint(1, 50) for x in xs]
-    axis.plot(xs, ys)
-    return fig
+	fig = Figure()
+ #   axis = fig.add_subplot(1, 1, 1)
+ #   xs = range(100)
+ #   ys = [random.randint(1, 50) for x in xs]
+ #   axis.plot(xs, ys)
+ 
+	df = pd.read_csv("https://statmeteo.000webhostapp.com/sensations/get-meteo.php")
+	df.columns = ['date_heure', 'station', 'vent', 'orientation', 'temperature']
+	df["date_heure"] = pd.to_datetime(df["date_heure"], format='%Y-%m-%d %H:%M')
+	df[["vent", "orientation", "temperature"]] = df[["vent", "orientation", "temperature"]].apply(pd.to_numeric)
+	station = "lusigny-sur-barse" #@param ["louviers", "mantes-la-jolie", "dreux", "montigny-le-bretonneux", "torcy", "montereau-fault-yonne", "lusigny-sur-barse"]
+
+	df_station = df[df['station'] == station]
+
+	is_semaine = df_station['date_heure'] > datetime.now() - timedelta(days=7)
+	df_station_semaine = df_station[is_semaine]
+
+	axis = fig.add_subplot(1, 1, 1)
+	xs = df_station_semaine['date_heure']
+	ys = df_station_semaine['vent']
+	axis.scatter(xs, ys)
+
+	return fig
